eec143fe140ace9e1f51a7cabd5218dd5929fdbd
msz13 <mat.szczecinski@gmail.com>
2023-01-31 23:47:19 +0100

    goal spike

--- goal_optimasier.py
+++ goal_optimasier.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import norm
 
 def WMax(t,W0, meanMax,stdMin,stdMax):
     return W0*np.e**((meanMax-(stdMin**2/2))*t + 3*stdMax*np.sqrt(t))
@@ -11,6 +12,9 @@ def deductE(row, logW0):
     e = diff[diff >= 0].min()
     return row - e
 
+def __prob2(W0, W1, mean, std, h):
+    return norm.pdf((np.log(W1/W0)-(mean-0.5*std**2)*h)/(std*np.sqrt(h)))
+
 def generateGrid(W0, T, iMax, minMean, minStd, maxMean, maxStd):
     grid = np.zeros((T,iMax))
     logW0 = np.log(W0)
@@ -21,3 +25,10 @@ def generateGrid(W0, T, iMax, minMean, minStd, maxMean, maxStd):
         row = deductE(row,logW0)
         grid[t-1] = row
     return np.exp(grid)
+
+def calculateTransitionPropabilities(W0, W1: np.array, mean,std, h):
+    p = np.fromiter((__prob2(W0,W,mean,std,h) for W in W1),np.float64)
+    return p/p.sum()
+
+def __calculate_values(portfolios,propabilities)
+

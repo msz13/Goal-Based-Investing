badf4e01486f3cab88224b992f506729aff929a5
msz13 <mat.szczecinski@gmail.com>
2023-02-20 00:26:51 +0100

    feat calculate_values_ for_ last_ period

--- investment_planner.py
+++ investment_planner.py
@@ -22,9 +22,6 @@ def deductE(row, logW0):
     e = diff[diff >= 0].min()
     return row - e
 
-def __prob2(W0, W1, mean, std, h):
-    return norm.pdf((np.log(W1/W0)-(mean-0.5*std**2)*h)/(std*np.sqrt(h)))
-
 def generateGrid(W0, T, iMax, infusions, goals, minMean, minStd, maxMean, maxStd) ->np.array:
     grid = np.zeros((T,iMax))
     logW0 = np.log(W0)
@@ -38,8 +35,11 @@ def generateGrid(W0, T, iMax, infusions, goals, minMean, minStd, maxMean, maxStd
         grid[t-1] = row
     return np.exp(grid)
 
-def calculateTransitionPropabilities(portfolioMeasures, W0, W1, h=1):
-    p = __prob2(W0,W1,portfolioMeasures[0], portfolioMeasures[1],1)
+def __prob2(W0, W1, mean, std, h):
+    return norm.pdf((np.log(W1/W0)-(mean-0.5*std**2)*h)/(std*np.sqrt(h)))
+
+def calculateTransitionPropabilities(portfolioMeasures, W0: int, W1: np.array, h=1):
+    p = __prob2(W0,W1,portfolioMeasures[0], portfolioMeasures[1],h)
     return p/p.sum()
 
 
@@ -47,11 +47,19 @@ def reachedGoal(W, goal=160):
     reachedGoal = W >= goal
     return reachedGoal.astype(int)
 
-def calculateTransitionPropabilitiesForAllPorfolios(portfolioMeasures, WT, WT1, h=1):
+def calculateValuesForLastPeriod(W, k):
+    values = np.zeros(len(W))
+    ki = k[0]
+    for i in range(len(W)):
+        values[i] = ki[1] if W[i] >= ki[0] else 0
+    return values
+
+
+def calculateTransitionPropabilitiesForAllPorfolios(portfolioMeasures, WT: np.array, WT1: np.array, h=1):
     i = len(WT1)
     probabilities = np.zeros((i,len(portfolioMeasures),i),np.float64)
     for i in range(i):
-        probabilities[i] = np.apply_along_axis(calculateTransitionPropabilities,1,portfolioMeasures, W0=WT, W1=WT1[i])
+        probabilities[i] = np.apply_along_axis(calculateTransitionPropabilities,1,portfolioMeasures, W0=WT[i], W1=WT1)
     return probabilities
 
 
@@ -62,13 +70,71 @@ def get_strategies(V):
     return porfolio_ids, maxes
 
 
+def generateGlidePath(W0, goal, T, portfolioMeasures):
+    iMax = 475
+    grid = generateGrid(W0,T,iMax,meanMin,stdMin,meanMax,stdMax)
+    strategies = np.zeros((T,iMax))
+    V = np.zeros((T,iMax))
+    probabilitiesT = np.zeros((T,iMax,iMax))
+
+    indexOf100 = np.where(grid[1]==100)
+
+    V[T-1] = reachedGoal(grid[T-1],goal)   
+
+    for t in range(T-2,0,-1):
+        probabilities = calculateTransitionPropabilitiesForAllPorfolios(portfolioMeasures,grid[t],grid[t+1])
+        VT = V[t+1] * probabilities        
+        porfolios_ids, VT_max = get_strategies(VT)
+        V[t] = VT_max  
+        strategies[t] = porfolios_ids  
+        chosen_propabilities = np.take_along_axis(probabilities,np.expand_dims(porfolios_ids,axis=(0,1)),1)
+        probabilitiesT[t] = chosen_propabilities[:,0,:]
+
+    return strategies, grid
+
 
 
 class InvestmentPlanner:
        
     def set_params(self, T: int, W0: float, infusion: float, infusionInterval: float, goals: np.array, portfolios: np.ndarray):
-        iMax = 8
+        self.iMax = 8
         infusions = np.full(T+1,infusion)   
         infusions[0] = 0    
-        self.grid = generateGrid(W0, T, iMax, infusions, goals, portfolios[0,0], portfolios[0,1], portfolios[-1,0], portfolios[-1,1] )
+        self.grid = generateGrid(W0, T, self.iMax, infusions, goals, portfolios[0,0], portfolios[0,1], portfolios[-1,0], portfolios[-1,1] )
+
+        self._strategies = np.zeros((T,self.iMax))
+        V = np.zeros((T,self.iMax))
+        self.probabilitiesT = np.zeros((T,self.iMax,self.iMax))
+
+        indexOf100 = np.where(self.grid[1]==100)
+
+        V[T-1] = reachedGoal(self.grid[T-1],goals[-1])   
+
+        for t in range(T-2,0,-1):
+            probabilities = calculateTransitionPropabilitiesForAllPorfolios(portfolios,self.grid[t],self.grid[t+1])
+            VT = V[t+1] * probabilities        
+            porfolios_ids, VT_max = get_strategies(VT)
+            V[t] = VT_max  
+            self.__strategies[t] = porfolios_ids  
+            chosen_propabilities = np.take_along_axis(probabilities,np.expand_dims(porfolios_ids,axis=(0,1)),1)
+            self.probabilitiesT[t] = chosen_propabilities[:,0,:]
+
+        self._calculate_cumulative_propabilities(T, self.probabilitiesT)
+    
+    def _calculate_cumulative_propabilities(self, T, probabilitiesT):
+        inputPropabilities = probabilitiesT
+        sums = inputPropabilities.sum(1)
+        cumulativeProbabilities = np.ones((T,self.iMax))
+        cumulativeProbabilities[0] = sums[1]
+        T=8
+        for t in range(2,T):
+            cumulativeProbabilities[t-1] = cumulativeProbabilities[t-2]*sums[t]
+        self.propabilities = cumulativeProbabilities
+         
+        
+    @property    
+    def glide_paths(self):
+        return self._strategies.T
+
+   
     
--- test_investment_planner.py
+++ test_investment_planner.py
@@ -1,5 +1,5 @@
 import pytest
-from investment_planner import InvestmentPlanner
+from investment_planner import InvestmentPlanner, calculateTransitionPropabilitiesForAllPorfolios, calculateValuesForLastPeriod
 import numpy.testing as npt
 import numpy as np
 
@@ -50,5 +50,61 @@ def test_should_simulateporfolio_values_with_infusions():
     assert pytest.approx(193.68,2) == grid[0,-1] '''
     npt.assert_array_almost_equal(expected_grid, grid, 4)
     
+def test_should_generate_glide_path():
+    
+    investent_planner = InvestmentPlanner()
+    portfolios = np.array([[0.0526, 0.0374], [0.07059443, 0.103057], [0.0886, 0.1954]])
+    W0 = 100
+    T = 2
+    infusion = 10
+    infustionInteval = 1
+    infusionGrowth = 0
+    goals = [0, 0, 65]
+    iMax = 8
+
+    investent_planner.set_params(T,W0, infusion, infustionInteval, goals, portfolios)
+
+    glide_paths = investent_planner.glide_paths
+
+    expected_glide_path = np.ones((iMax,T))
+
+    assert glide_paths.shape == (iMax,T)
+    assert npt.assert_almost_equal(glide_paths, expected_glide_path)
+
+def test_generate_propabilities():
+    portfolios = portfolios = np.array([[0.0526, 0.0374], [0.07059443, 0.103057], [0.0886, 0.1954]])
+    WT = np.array([90,95,100,110])
+    WT1 = np.array([90,100,110,120])
+    i = len(WT1)
+    propabilities = calculateTransitionPropabilitiesForAllPorfolios(portfolios,WT,WT1)
+    expected_propabilities = np.array([[[0.514, 0.485, 0.,    0.   ],
+                                        [0.361, 0.409, 0.186, 0.043],
+                                        [0.288, 0.302, 0.245, 0.165]],
+
+                                        [[0.017, 0.945, 0.038, 0.   ],
+                                        [0.205, 0.397, 0.293, 0.105],
+                                        [0.238, 0.289, 0.269, 0.204]],
+
+                                        [[0., 0.427, 0.57, 0.003],
+                                        [0.099, 0.32, 0.375, 0.205],
+                                        [0.194, 0.272, 0.288, 0.246]],
+
+                                        [[0., 0., 0.372, 0.627],
+                                        [0.017, 0.139, 0.384, 0.459],
+                                        [0.127, 0.232, 0.311, 0.33 ]]])                                       
+
+    npt.assert_almost_equal(propabilities,expected_propabilities,3)
+
+def test_should_calclulate_values_for_last_period():
+    k = [[105,100]]
+    wealthInT = [90,95,100,105,110]
+    expectedValues = [0,0,0,100,100]
+
+    V = calculateValuesForLastPeriod(wealthInT,k)    
+
+    npt.assert_array_equal(V,expectedValues)
+
+
+
 
 

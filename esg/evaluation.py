2a77ba0990ebb0013a8cd47066374740bf5f5887
msz13 <mat.szczecinski@gmail.com>
2023-09-20 23:15:22 +0200

    acwi geometric brownian motion

--- esg/evaluation.py
+++ esg/evaluation.py
@@ -5,6 +5,7 @@ def fanchart(hist,percentiles):
     length = 180
     x = np.linspace(0,length+1,length+1)
     fig,ax = plt.subplots(figsize=(12,6))
-    ax.fill_between(x=x,y1=percentiles[0],y2=percentiles[4], color='blue', alpha=0.1)
-    ax.fill_between(x=x,y1=percentiles[1],y2=percentiles[3], color='blue', alpha=0.2)
+    ax.fill_between(x=x,y1=percentiles[0],y2=percentiles[6], color='blue', alpha=0.1)
+    ax.fill_between(x=x,y1=percentiles[1],y2=percentiles[5], color='blue', alpha=0.2)
+    ax.fill_between(x=x,y1=percentiles[2],y2=percentiles[4], color='blue', alpha=0.3)
     ax.plot(percentiles[2],color='blue')

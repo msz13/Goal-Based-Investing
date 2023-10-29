ecd0fae5c98685ccd067d484c47456e23fde8eb3
msz13 <mat.szczecinski@gmail.com>
2023-10-29 22:19:06 +0100

    finish msci model with normal distribution

--- esg/preproccessing.py
+++ esg/preproccessing.py
@@ -2,6 +2,7 @@ import requests
 from io import StringIO
 import pandas as pd
 import yfinance as yf
+import numpy as np
 
 def loadStooqData(ticker: str,start, frequency='d'):
     url = f'https://stooq.pl/q/d/l/?s={ticker}&i={frequency}'
@@ -17,3 +18,19 @@ def loadYahooData(ticker, start, frequency = '1d'):
     hist = ticker.history(start=start,interval=frequency)
     price = hist['Close']
     return price 
+
+def log_returns(series, freg='m'):
+    shift = {
+        'm': 1,
+        'q': 4,
+        'y': 12
+    }
+    return np.log(series/series.shift(shift[freg])).dropna()
+
+
+def corr_to_cov(corr_matrix, std_devs):
+    """
+    Converts vector of standard deviations and correlation matrix into covariance matrix
+    """
+    D = np.diag(std_devs)
+    return D @ corr_matrix @ D

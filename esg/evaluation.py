8708d690e008b63767704a3f636859b079ad9361
msz13 <mat.szczecinski@gmail.com>
2023-11-05 22:35:54 +0100

    working on evaluation functions

--- esg/evaluation.py
+++ esg/evaluation.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from preproccessing import assets_performance
 
 def fanchart(hist,percentiles):
-    length = 180
+    length = 60
     x = np.linspace(0,length+1,length+1)
     fig,ax = plt.subplots(figsize=(12,6))
     ax.fill_between(x=x,y1=percentiles[0],y2=percentiles[6], color='blue', alpha=0.1)
     ax.fill_between(x=x,y1=percentiles[1],y2=percentiles[5], color='blue', alpha=0.2)
     ax.fill_between(x=x,y1=percentiles[2],y2=percentiles[4], color='blue', alpha=0.3)
     ax.plot(percentiles[2],color='blue')
+
+def describe_scenarios_vertically(scenarios: pd.DataFrame):
+    """"
+    Returns mean, standard devation, percentiles of scenarios performence: 
+    annualised mean, std, skew, kurtosis, sharp ratio, maxdrawdown
+    """
+    return assets_performance(scenarios).T.describe()
+    
--- esg/preproccessing.py
+++ esg/preproccessing.py
@@ -19,6 +19,14 @@ def loadYahooData(ticker, start, frequency = '1d'):
     price = hist['Close']
     return price 
 
+def calculate_returns(series, freg='m'):
+    shift = {
+        'm': 1,
+        'q': 4,
+        'y': 12
+    }
+    return series.pct_change(shift[freg]).dropna()
+
 def log_returns(series, freg='m'):
     shift = {
         'm': 1,
@@ -34,3 +42,27 @@ def corr_to_cov(corr_matrix, std_devs):
     """
     D = np.diag(std_devs)
     return D @ corr_matrix @ D
+
+def annualised_mean(returns):
+    return returns.mean() * 12
+
+def annualised_sigma(returns):
+    return returns.std() * 12**(1/2)
+
+def foreign_asset_std(asset_std, currency_std, corr):
+    variance = asset_std**2 + currency_std**2 + 2*corr*asset_std*currency_std
+    return np.sqrt(variance)
+
+def sharp_ratio(returns, risk_free_rate=0.03):
+    return (annualised_mean(returns)-risk_free_rate)/returns.std()
+
+def max_drawdown(returns):
+    wealth_index = np.exp(returns).cumprod()
+    rolling_max = wealth_index.cummax()
+    drawdown = wealth_index/rolling_max -1
+    return drawdown.min()
+    
+def assets_performance(returns: pd.DataFrame):
+    #return returns.agg(['mean', 'std', 'median', 'skew', 'kurtosis', sharp_ratio, max_drawdown])
+    return returns.agg([annualised_mean, annualised_sigma, 'skew', 'kurtosis', sharp_ratio, max_drawdown])
+

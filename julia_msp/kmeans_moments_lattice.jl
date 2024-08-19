a6a777fe6b5ee556bb771ba25e6d0eff66fba9d1
msz13 <mat.szczecinski@gmail.com>
2024-08-19 23:27:17 +0200

    generate lattice - start work

--- julia_msp/kmeans_moments_lattice.jl
+++ julia_msp/kmeans_moments_lattice.jl
@@ -1,8 +1,8 @@
-module Lattice
+module LatticeGeneration
 
 using StatsBase, Clustering
 
-export cluster
+export cluster, clusters_probs, generate_lattice, Lattice
 
 function cluster(data, n_clusters)
     dt = StatsBase.fit(ZScoreTransform, data; dims=2, center=true, scale=true)
@@ -11,11 +11,37 @@ function cluster(data, n_clusters)
     clusters = kmeans(standardized,n_clusters)
 
     destandarised = StatsBase.reconstruct(dt, clusters.centers)
-
-    probs = counts(clusters)/sum(counts(clusters)) 
-
-    return destandarised, probs
+    
+    return transpose(destandarised), assignments(clusters)
 end
 
+function clusters_probs(cluster_assignments)
+    counts(cluster_assignments) ./length(cluster_assignments)
+end
+
+mutable struct Lattice
+    states:: Array{Float64,3}
+end
+
+function generate_lattice(scenarios:: Array{Float64,3}, n_nodes, n_stages)
+
+    s = sum(scenarios[:,:,21:25]; dims=3)
+    s = dropdims(s; dims= 3)
+
+    n = 3 # number of assets
+    
+    states =zeros(n_stages,n_nodes,n)
+    probablities = []
+
+    states_one, clusters = cluster(s,n_nodes)
+    probs = clusters_probs(clusters)
+
+    states[1,:,:] .= states_one
+    append!(probablities,probs)
+
+    return Lattice(states), probablities
+
+
+end
 
 end

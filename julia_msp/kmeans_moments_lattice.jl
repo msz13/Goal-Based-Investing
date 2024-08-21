ecff078f6958147318fb288f7787a746279f31ba
msz13 <mat.szczecinski@gmail.com>
2024-08-21 23:44:03 +0200

    kmeans_moments fix + start successors_probs

--- julia_msp/kmeans_moments_lattice.jl
+++ julia_msp/kmeans_moments_lattice.jl
@@ -2,7 +2,7 @@ module LatticeGeneration
 
 using StatsBase, Clustering
 
-export cluster, clusters_probs, generate_lattice, Lattice
+export cluster, clusters_probs, generate_lattice, Lattice, successors_probs
 
 function cluster(data, n_clusters)
     dt = StatsBase.fit(ZScoreTransform, data; dims=2, center=true, scale=true)
@@ -19,6 +19,10 @@ function clusters_probs(cluster_assignments)
     counts(cluster_assignments) ./length(cluster_assignments)
 end
 
+function successors_probs(successors_assignments, ancessors_assignments, node)
+     return clusters_probs(ancessors_assignments[successors_assignments .== node])
+end
+
 mutable struct Lattice
     states:: Array{Float64,3}
     probs:: Array{Matrix{Float64},1}
@@ -33,11 +37,11 @@ function generate_lattice(scenarios:: Array{Float64,3}, n_nodes, n_stages)
     n = 3 # number of assets
     
     states =zeros(n_stages,n_nodes,n)
-    assignments = zeros_
+   
     probablities = Array{Float64,2}[[1.0]']
 
     for t in 1:n_stages
-        stage_states, clusters_assignments = cluster(scenarios[:,:,1],n_nodes)
+        stage_states, clusters_assignments = cluster(scenarios[:,:,t],n_nodes)
         states[t,:,:] .= stage_states
     end
 

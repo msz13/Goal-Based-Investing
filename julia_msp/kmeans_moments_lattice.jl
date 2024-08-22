e2d689c1b5ccd06ef1f70110f724db742595883c
msz13 <mat.szczecinski@gmail.com>
2024-08-22 23:37:19 +0200

    finish kmeans with cluster probs

--- julia_msp/kmeans_moments_lattice.jl
+++ julia_msp/kmeans_moments_lattice.jl
@@ -15,12 +15,24 @@ function cluster(data, n_clusters)
     return transpose(destandarised), assignments(clusters)
 end
 
-function clusters_probs(cluster_assignments)
-    counts(cluster_assignments) ./length(cluster_assignments)
+function clusters_probs(cluster_assignments:: Vector{Int64}, n_nodes:: Int64)
+    counts(cluster_assignments, n_nodes)/length(cluster_assignments)
+   
 end
 
-function successors_probs(successors_assignments, ancessors_assignments, node)
-     return clusters_probs(ancessors_assignments[successors_assignments .== node])
+"""
+    successors_probs(successors_assignments, ancessors_assignments, n_nodes, node)
+
+Calculates next state probabilities for previous stage node.
+
+successors_assignments: clusters of scenarios of t stage
+ancessors_assignments: clusters of scenarios of t + 1 stage
+n_nodes - number of nodes
+node: node for which next stage probabilies are calculates
+
+"""
+function successors_probs(ancessors_assignments, successors_assignments, n_nodes, node)
+     return clusters_probs(successors_assignments[ancessors_assignments .== node],n_nodes)
 end
 
 mutable struct Lattice
@@ -34,24 +46,34 @@ end
 function generate_lattice(scenarios:: Array{Float64,3}, n_nodes, n_stages)
 
    
-    n = 3 # number of assets
+    n, n_scenarios, T = size(scenarios)
     
-    states =zeros(n_stages,n_nodes,n)
+    states =zeros(n_stages, n_nodes, n)
    
     probablities = Array{Float64,2}[[1.0]']
 
+    clusters_assignments = zeros(Int64, T, n_scenarios)
+
     for t in 1:n_stages
-        stage_states, clusters_assignments = cluster(scenarios[:,:,t],n_nodes)
+        stage_states, clusters_assignments[t,:] = cluster(scenarios[:,:,t],n_nodes)
         states[t,:,:] .= stage_states
     end
 
+    #calculate probabilities for first stage
     
-    #probs = clusters_probs(clusters)
+    probs = clusters_probs(clusters_assignments[1,:], n_nodes)
+    
+    push!(probablities,probs')
 
-    
-    #push!(probablities,probs')
+    #calculate probabilities for first stage
 
-    
+    for t in 2:T
+        stage_probs = zeros(n_nodes, n_nodes)
+        for node in 1:n_nodes
+            stage_probs[node,:] .= successors_probs(clusters_assignments[t-1,:],clusters_assignments[t,:],n_nodes,node)
+        end
+        push!(probablities,stage_probs)
+    end
 
     return Lattice(states, probablities)
 

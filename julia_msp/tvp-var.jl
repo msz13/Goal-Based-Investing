dd3e3d64f7f0e081565444b234c6506589d54237
msz13 <mat.szczecinski@gmail.com>
2024-10-24 22:35:23 +0200

    ttvp var

--- julia_msp/tvp-var.jl
+++ julia_msp/tvp-var.jl
@@ -1,10 +1,45 @@
 using Distributions
+using LinearAlgebra
+
 
 struct SVparams
     μ 
     ρ 
 
 end
+
+"""
+generate_coeffs(Β0, p, ν0, ν1, h)
+samples  coeeficents for h periods
+
+Β0: initial Cooeficeint
+sp: probability of break in time varing cooefs
+ν0: coeeficients volatility for steady state. Close to zero.
+ν1: coeeficients volatility for time varing state. 
+h: number of perios
+
+"""
+function generate_coeffs(Β0, sp, ν0, ν1, h)
+    j = length(Β0)
+
+    result = zeros(j,h+1)
+    result[:,1] = Β0
+
+    for t in 2:(h+1)
+        s = [rand(Bernoulli(p)) for p in sp] 
+        Θ = [s[i]*ν1[i] + (1-s[i])*ν0[i] for i in 1:j] 
+    
+        V = diagm(Θ) * Matrix(I, j, j)
+       
+        result[:,t] = result[:, t-1] .+ rand(MvNormal(zeros(j), V),1)
+
+    end
+    
+    return result[:,2:end]
+
+end
+
+
 
 
 function step(x, Βtm1, p, ν0,  ν1, htm1, SVparams)

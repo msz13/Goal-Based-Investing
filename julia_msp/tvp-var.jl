c1d549481ffae039666dc45c731825674cb69a57
msz13 <mat.szczecinski@gmail.com>
2024-11-11 22:43:25 +0100

    kalman filter in progress cd

--- julia_msp/tvp-var.jl
+++ julia_msp/tvp-var.jl
@@ -106,9 +106,8 @@ function posterior()
 end
  =#
 
- residuals(Y, X, Β) = Y .- Β * X 
+ 
 
- k_gain(P_predicted, X, Σ) =  P_predicted * X' * inv(X * P_predicted * X' .+ Σ)
  
 function kalman_step(Sm1, Pm1, Y, X, Σ, Q)
     
@@ -116,23 +115,51 @@ function kalman_step(Sm1, Pm1, Y, X, Σ, Q)
     S = Sm1
     P1 = Pm1 +  Q
 
-    F = X * P1 * X' + Σ  #state transision
+    #= F = X * P1 * X' + Σ  #state transision
     K = P1 * X' * inv(F) # kalman gain
 
     res = Y .- S * X #residuals
 
     Β_f = S + K * res #Beta measurement
-    P_f = P1 - K * X * P1 #covariance measurmenet
+    P_f = P1 - K * X * P1 #covariance measurmenet =#
 
-    return Β_f, P_f
+    return P1 #Β_f, P_f
 
 end
 
+residuals(Y, X, Β) = Y .- Β ⋅ X 
+k_gain(X, P1, Σ) = P1 * X' *  inv(X * P1 * X' + Σ)
+update_state(S1, K, res) = S1 .+ (K * res)'
+update_cov(P1, K, X) = P1 - K'*X'*P1 
 
- function kalmanFilter(X, Y, Β0, P0, Σ, ν)
+
+
+function kalman_step2(Sm1:: Matrix{Float64}, Pm1:: Matrix{Float64}, Y:: Float64, X:: Matrix{Float64}, Σ:: Vector{Float64}, Q:: Matrix{Float64})
+    
+    #predict state
+    S1 = Sm1
+    P1 = Pm1 +  Q
+
+    F = X * P1 * X' + Σ  
+    K = P1 * X' *  inv(F)
+
+    
+    res =  Y .- X * S1' #residuals 
+   
+
+    Β_f =  S1 + K * res #Beta measurement Update state S +
+
+    
+    #P_f = P1 - K * X * P1 #covariance measurmenet  
+
+    return K #Β_f, P_f
+
+end
+
+function kalmanFilter(X, Y, Β0, P0, Σ, ν)
     T,i = size(X)
     
-    S_filtered = zeros(T+1, i,i)
+    S_filtered = zeros(T+1, i, i)
     P_filtered = zeros(T+1, i, i)
 
     S_filtered[1,:,:] .= Β0
@@ -147,3 +174,39 @@ end
     return S_filtered, P_filtered
 
  end 
+
+
+ function kalmanFilter2(X, Y, Β0, P0, Σ, ν)
+    T,i = size(X)
+    
+    S_filtered = zeros(T+1, i)
+    P_filtered = zeros(T+1, i, i)
+
+    S_filtered[1,:] = Β0
+    P_filtered[1,:,:] = P0
+
+   for t in 2:T+1
+        
+        S_filtered[t,:], P_filtered[t,:] .= kalman_step(S_filtered[t-1,:], P_filtered[t-1, :, :], Y[t-1], X[t-1,:], Σ, ν)
+
+    end  
+     
+    return S_filtered, P_filtered 
+
+ end 
+
+function smoother(S_filtered, P_filtered)
+
+    T, i, j = size(S_filtered)
+    S = zeros(T, i, j)
+    P = zeros(T, i, j)
+
+    S[end, :, :] .= rand(MvNormal(S_filtered[end, :, :], P_filtered[end, :, :])) 
+    P[end, :, :] .= P_filtered[end, :, :]
+    
+    S[end-1,:, :] .= 
+    
+ 
+    return S, P
+
+end

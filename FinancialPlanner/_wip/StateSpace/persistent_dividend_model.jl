324204b0bad570e8f02d5d1adf0622dfd4b070fe
msz13 <mat.szczecinski@gmail.com>
2025-04-09 22:16:28 +0200

    state space inference spike 2

--- FinancialPlanner/_wip/StateSpace/persistent_dividend_model.jl
+++ FinancialPlanner/_wip/StateSpace/persistent_dividend_model.jl
@@ -7,6 +7,8 @@ function buildF(Θ1, Θ2)
     ]
 end
 
+
+
 function buildH(n, rho, Θ1, Θ2)
     Hp = [1 / (1 - ρ), -1 / (1 - ρ), 0, Θ1[1] / (1 - ρ * Θ1[1]), -Θ1[2] / (1 - ρ * Θ1[2]), 0, -Θ1[4] / (1 - ρ * Θ1[4]), 0, Θ2[1] / (1 - ρ * Θ2[1]), -Θ2[2] / (1 - ρ * Θ2[2]), 0, -Θ2[4] / (1 - ρ * Θ2[4]), 0]'
     Hin = [0, 1, 1, 0, (1 / n) * ((1 - Θ1[2]^n) / (1 - Θ1[2])) * Θ1[2], (1 / n) * ((1 - Θ1[3]^n) / (1 .- Θ1[3])) * Θ1[3], 0, (1 / n) * ((1 - Θ1[5]^(n - 1)) / (1 .- Θ1[5])) * Θ1[5], 0, (1 / n) * ((1 - Θ2[2]^n) / (1 - Θ2[2])) * Θ2[2], (1 / n) * ((1 - Θ2[3]^n) / (1 .- Θ2[3])) * Θ2[3], 0, (1 / n) * ((1 - Θ2[5]^(n - 1)) / (1 .- Θ2[5])) * Θ2[5]]'
@@ -28,14 +30,15 @@ end
 
 function buildQ(Q)
     result= zeros(13, 13)
-    result[1:8, 1:8] = Q
+    cov_matrix = diagm(Q)
+    result[1:8, 1:8] = cov_matrix
     return result
 end
 
 
 
 
-function persistent_dividend_SSM(params::NamedTuple{(:Θ1, :Θ2, :R, :Q)})
+function PersistentDividendSSM(params::NamedTuple{(:Θ1, :Θ2, :R, :Q)})
     F = buildF(params.Θ1, params.Θ2)
     n = 40
     ρ = 0.967
@@ -47,4 +50,16 @@ function persistent_dividend_SSM(params::NamedTuple{(:Θ1, :Θ2, :R, :Q)})
     model = LinearGaussianStateSpaceModel(zeros(13), zeros(13, 13), F, zeros(13), Q, H, Rmatrix)
     
     return model
+end
+
+function persistent_dividend_ssm(params_vector:: AbstractVector)
+
+    Θ1 = params_vector[1:5]
+    Θ2 = params_vector[6:10]
+    R = params_vector[11:15]
+    Q = params_vector[16:23]
+
+    params = (Θ1 = Θ1, Θ2 =Θ1, R = R, Q = Q)
+    return PersistentDividendSSM(params)    
+
 end
--- FinancialPlanner/_wip/StateSpace/persistent_dividend_script.jl
+++ FinancialPlanner/_wip/StateSpace/persistent_dividend_script.jl
@@ -11,58 +11,63 @@ includet("persistent_dividend_model.jl")
 
 R = [1.45*10^-1, 1.59*10^-14, 3.01*10^-13, 7.43*10^-10, 3.78*10^-12 ]
 
-Q = diagm([4.66 * 10^-2, 2.34 * 10^-2, 5.51 * 10^-2, 2.2, 5.0 * 10^-1, 2.53 * 10^-1, 62.11, 2.09])
-#Q = [4.66 * 10^-2, 2.34 * 10^-2, 5.51 * 10^-2, 2.2, 5.0 * 10^-1, 2.53 * 10^-1, 62.11, 2.09]
+#Q = diagm([4.66 * 10^-2, 2.34 * 10^-2, 5.51 * 10^-2, 2.2, 5.0 * 10^-1, 2.53 * 10^-1, 62.11, 2.09])
+Q = [4.66 * 10^-2, 2.34 * 10^-2, 5.51 * 10^-2, 2.2, 5.0 * 10^-1, 2.53 * 10^-1, 62.11, 2.09]
 
 sqrt.(R) 
 sqrt.(Q) ./ 100 * 2
 
-model = persistent_dividend_SSM(NamedTuple{(:Θ1, :Θ2, :R, :Q)}([Θ1, Θ2, R, Q]))
+#model = PersistentDividendSSM(NamedTuple{(:Θ1, :Θ2, :R, :Q)}([Θ1, Θ2, R, Q]))
+
+params_vec = [Θ1; Θ1; R; Q]
+model = persistent_dividend_ssm(params_vec)
+
 
 rng = StableRNG(1234)
 x, y = generate_data(rng, model, 100)
 
 
 
+prior = Product([fill(Normal(), 10); fill(truncated(Cauchy(.01, .03); lower=0), 13)])                 
 
-prior = product_distribution(product_distribution(Normal(1,0), Normal(1,0), Normal(1,0), Normal(), Normal(),Normal(), Normal(),Normal()), 
-                             product_distribution(Normal(1,0), Normal(1,0), Normal(1,0), Normal(), Normal(),Normal(), Normal(),Normal()),
-                             product_distribution(truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0)),
-                             product_distribution(truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0), truncated(Cauchy(.01, .03); lower=0))
-                            )
+rand(prior)
+
+
+
+pdf(prior, params_vec)
+
+
 
 ll_pmmh = PMMH(
                 5_000,
-                θ -> MvNormal(θ, Matrix((0.1)*I(32))),
-                θ -> model,
+                θ -> MvNormal(θ, Matrix((0.1)*I(23))),
+                θ -> persistent_dividend_ssm(θ),
                 prior
                 )
 
 
-kf_pmmh = sample(rng, ll_pmmh, y, KF(); burn_in=1000)
-
-kf_pmmh[1]
+kf_pmmh = sample(rng, ll_pmmh, y, KF(); burn_in=3000)
 
 a = mean(getproperty.(kf_pmmh, :params))
 
 
+pf_pmmh = sample(rng, ll_pmmh, y, PF(256, 1.0); burn_in=1000)
 
-g = rand(InverseWishart(100,Matrix(I(3))),1000)
+b = mean(getproperty.(pf_pmmh, :params))
 
 
+params_vec'
+a'
+b'
 
-g = rand(InverseGamma(80, 2.5), 1000)
+println(round.(params_vec, digits=4))
+println(round.(a, digits=4))
+println(round.(b, digits=4))
 
-mean(g)
-quantile(g, [0, .05, .25, .50, .75, .95, 1.])
+#true params
+[0.333, 0.343, 0.003, 0.528, 0.234, 0.333, 0.343, 0.003, 0.528, 0.234, 0.145, 0.0, 0.0, 0.0, 0.0, 0.0466, 0.0234, 0.0551, 2.2, 0.5, 0.253, 62.11, 2.09]
+#a
+[0.505, 0.2374, 0.0428, -0.4, 0.3867, -2.6465, -1.7975, -0.899, 3.3745, 1.487, 3.2911, 0.0791, 0.2928, 0.1183, 0.2123, 0.4743, 0.2578, 0.2549, 2.375, 1.0027, 0.1415, 1.0267, 0.3902]
 
-g = rand(truncated(Cauchy(.01, .03); lower=0), 1000)
-
-mean(g)
-quantile(g, [0, .05, .25, .50, .75, .95, 1.])
-
-g = rand(truncated(Normal(); lower=0), 1000)
-
-mean(g)
-quantile(g, [0, .05, .25, .50, .75, .95, 1.])
-
+#b
+[1.756, 0.3019, -0.0992, 0.5201, -1.0967, 0.1373, 0.2291, -0.7503, 0.2902, 0.6158, 0.6958, 0.0252, 0.0928, 0.0341, 0.3044, 0.0072, 0.0533, 0.0607, 0.0145, 0.0404, 0.0153, 0.0113, 0.0383]

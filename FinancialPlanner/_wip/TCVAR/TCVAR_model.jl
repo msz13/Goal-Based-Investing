f4e426a875b58e24337c78106d402c9395dd8697
msz13 <mat.szczecinski@gmail.com>
2025-08-11 23:36:53 +0200

    tcvar cd

--- FinancialPlanner/_wip/TCVAR/TCVAR_model.jl
+++ FinancialPlanner/_wip/TCVAR/TCVAR_model.jl
@@ -1,4 +1,4 @@
-function TCVAR(
+#= function TCVAR(
     params:: NamedTuple,
     initial_observation:: Vector{Float64},
     initial_covariance:: Matrix{Float64}
@@ -29,3 +29,27 @@ function TCVAR(
 end
  
 
+ =#
+
+ # Build SSM from parameter matrices (accepts full 2x2 Q_trend and Q_cycle)
+function TCVAR(phi::Matrix{Float64}, Q_trend::Matrix{Float64}, Q_cycle::Matrix{Float64})
+    # State ordering: [trend_infl, trend_rate, cycle_infl, cycle_rate]
+    A = zeros(4,4)
+    A[1:2, 1:2] .= I(2)            # trend random walk
+    A[3:4, 3:4] .= phi             # cycles VAR(1)
+
+    b = zeros(4)
+          
+    Q = zeros(4,4)
+    Q[1:2, 1:2] .= Q_trend
+    Q[3:4, 3:4] .= Q_cycle
+
+    H = [1.0 0.0 1.0 0.0; 0.0 1.0 0.0 1.0]
+    R = zeros(2, 2)
+
+    x0 = zeros(4)
+    Σ0 = 100.0 * Matrix(I, 4, 4)
+     
+    return create_homogeneous_linear_gaussian_model(x0, Σ0, A, b, Q, H, c, R)
+
+end

fbdefcb32f2ac8428d13b566b2fd59a13c3f35ac
msz13 <mat.szczecinski@gmail.com>
2025-09-01 23:07:42 +0200

    tcvar start integrate var params sampler to gibs sampler

--- FinancialPlanner/_wip/TCVAR/gibbs_sampler.jl
+++ FinancialPlanner/_wip/TCVAR/gibbs_sampler.jl
@@ -9,45 +9,57 @@ function gibs_sampler(data, priors, n_samples)
     n_time_steps, n_obs = size(data) 
     n_trends = 2
     n_states = n_trends + n_obs
+    p = 1 #number of var lags
+    k = n_obs * p #number of var variables 
+
+
 
     #posterior degrees pf freedom for trend covariance matrix
-    d_post = n_time_steps - 1 + priors.trend_covariance_df
+    dτ_post = n_time_steps - p + priors.trend_covariance_df
 
-    true_var_coeff = [.3 .1;  .4 .6]
-    true_trend_covariance =  diagm([.02/10, .01/10] .^2) 
+    #posterior degrees pf freedom for cycle covariance matrix
+    dc_post = n_time_steps - p + priors.cycle_covariance_df
 
-    p = -0.7 * .015 * 0.011
+    #prior of covariance matrix, for lag one
+    λ = priors.cycle_coeff_shrinkage_param
+    Ω = [λ^2, λ^2] ./ diag(priors.cycle_covariance_scale)
+    Ω_inv = inv(Diagonal(Ω))
 
-    true_cycle_covariance = [(.015)^2 p
-                             p (.011)^2]
-
-     
-    
-    true_initial_trend_covariance = diagm([.01, .01]) #Matrix(I, 2,2)
-    true_initial_cycle_mean = [.0, .0]
-    true_initial_cycle_covariance = [(.015)^2 0
-                                     0 (.011)^2]   
+    initial_cycle_covariance = priors.cycle_covariance_scale
     
     # Storage for sampled states and variables
     state_smoothed_samples = zeros(n_samples+1, n_time_steps, n_states) #TODO uporzadkowac, co jest samplowane dla states dla initial
     sampled_trend_covariance = zeros(n_samples+1, n_trends, n_trends)
+    
+    betas = zeros(n_samples, k*k)
+    sigmas = zeros(n_samples, n_obs, n_obs)
 
+    #sample initial parameters values form prior distribution
     sampled_trend_covariance[1, :, :] = rand(InverseWishart(priors.trend_covariance_df, priors.trend_covariance_mean))
+    
+    betas[1, :] = MvNormal(vec(priors.cycle_coeff_mean), reshape(vec(diagm(Ω)), 4,4))
+    sigmas[1, :, :] = rand(InverseWishart(priors.cycle_covariance_df, priors.cycle_covariance_scale)) 
     
 
     for s in 2:n_samples
-        model = tc_var(true_var_coeff, 
+        model = tc_var(
+                reshape(betas[s, :], n_obs, k), 
                 sampled_trend_covariance[s-1,:,:], 
-                true_cycle_covariance, 
+                sigmas[s,:,:],       
                 priors.initial_trend_mean, 
-                true_initial_cycle_mean, 
-                true_initial_trend_covariance, 
-                true_initial_cycle_covariance
+                priors.initial_cycle_mean,
+                priors.initial_trend_covariance,
+                initial_cycle_covariance                              
                 )
         
         state_smoothed_samples[s, :, :] = carter_kohn_sampler(model, data)
 
-        sampled_trend_covariance[s, :, :] = rand(covariance_posterior(state_smoothed_samples[s, :, [1,2]], priors.trend_covariance_mean, d_post))
+        trends_states = state_smoothed_samples[s, :, [1,2]]
+        cycle_states =  state_smoothed_samples[s, :, [3,4]]
+
+        sampled_trend_covariance[s, :, :] = rand(covariance_posterior(trends_states, priors.trend_covariance_mean, dτ_post))
+
+        betas[s,:], sigmas[s, :, :] = sample_var_params(cycle_states, 1, priors.cycle_coeff_mean, Ω_inv, priors.cycle_covariance_scale, dc_post)
 
     end
 
